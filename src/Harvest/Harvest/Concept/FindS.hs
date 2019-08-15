d003558f3e8376fba0058fb8414f5f9be4836d1b
Ben Lippmeier <benl@ouroborus.net>
2019-08-15 16:06:46 +0200

    finds: add demo

---
+++ src/Harvest/Harvest/Concept/FindS.hs
@@ -0,0 +1,82 @@
+
+-- | FindS Concept Learning Algorithm.
+--
+--   From Machine Learning, Tom M. Mitchell, 1997.
+--
+module Harvest.Concept.FindS
+        ( Constraint    (..)
+        , Hypothesis    (..)
+        , Instance      (..)
+        , findS)
+where
+import qualified Data.List      as L
+
+-- | A constraint on a particular value.
+data Constraint a
+        = None          -- ^ No value is acceptable.
+        | Exact a       -- ^ Need this exact value.
+        | Any           -- ^ Any value is acceptable.
+        deriving (Eq, Show)
+
+
+-- | A hypothesis is a list of constraints.
+data Hypothesis a
+        = Hypothesis [Constraint a]
+        deriving (Eq, Show)
+
+
+-- | An instance.
+data Instance a
+        = Instance   [a]
+        deriving (Eq, Show)
+
+
+-- | Weaken a single constraint, given an example attribute.
+weakenFromAttribute :: Eq a => Constraint a -> a -> Constraint a
+weakenFromAttribute cc x
+ = case cc of
+        None            -> Exact x
+        Exact x'
+         | x == x'      -> Exact x
+         | otherwise    -> Any
+        Any             -> Any
+
+
+-- | Weaken a hypothesis, given an example instance.
+weakenFromInstance  :: Eq a => Hypothesis a -> Instance a  -> Hypothesis a
+weakenFromInstance (Hypothesis cs) (Instance xs)
+        = Hypothesis (zipWith weakenFromAttribute cs xs)
+
+
+-- | The Find-S algorithm learns a maximally specific hypothesis from
+--   the given set of training data.
+--
+--   We start with the most specific hypothesis, which is that no attribute
+--   value is acceptable, then progressively weaken the hypothesis so that
+--   it covers all positive training examples.
+--
+--   Limitations of the basic algorithm:
+--    * Assumes the training data is noiseless.
+--    * Does not use negative examples, which only works if data is noiseless.
+--    * Returns the most specific hypothesis, we might not want that.
+--
+--   Limitations of the implementation:
+--   * The arity of each instance must be the same.
+--
+findS :: Eq a => [(Instance a, Bool)] -> Hypothesis a
+findS train@(i0 : _)
+ = let
+        -- Get the arity of the first instance,
+        -- and assume all the others have the same arity.
+        (Instance as0, _) = i0
+        arity           = length as0
+
+        -- Start with the most specific hypothesis.
+        hyp0            = Hypothesis (replicate arity None)
+
+        -- We only use the positive training examples.
+        instPos         = [ i | (i, b@True)     <- train]
+
+        -- Successively refine the hypothesis using the training examples.
+   in   L.foldl' weakenFromInstance hyp0 instPos
+
---
+++ src/main/finds/Main.hs
@@ -0,0 +1,62 @@
+
+-- | The Find-S algorithm learns a maximally specific hypothesis from
+--   the given set of training data.
+--
+--   We start with the most specific hypothesis, which is that no attribute
+--   value is acceptable, then progressively weaken the hypothesis so that
+--   it covers all positive training examples.
+--
+--   Limitations of the basic algorithm:
+--    * Assumes the training data is noiseless.
+--    * Does not use negative examples, which only works if data is noiseless.
+--    * Returns the most specific hypothesis, we might not want that.
+--
+--   Limitations of the implementation:
+--   * The arity of each instance must be the same.
+--
+--   Input format is a CSV file where the first column in each row gives
+--   a boolean result and the rest give the other attributes for each
+--   instance.
+--
+--   Example:
+--
+--   Enjoy,Sky,Air,Humidity,Wind,Water,Forecast
+--   true,sunny,warm,normal,strong,warm,same
+--   true,sunny,warm,high,strong,warm,same
+--   false,rainy,cold,high,strong,warm,change
+--   true,sunny,warm,high,strong,cool,change
+--
+import qualified Harvest.Concept.FindS  as H
+
+import Data.Text                        (Text)
+import qualified System.Environment     as System
+import qualified Text.Comma             as Comma
+import qualified Data.Text.IO           as T
+
+
+main
+ = do   args    <- System.getArgs
+        case args of
+         [fileName]     -> runMain fileName
+         _              -> error "usage: harvest-finds FILE.csv"
+
+runMain fileName
+ = do   file    <- T.readFile fileName
+        case Comma.comma file of
+         Left err       -> error err
+         Right (lHeader:lsRow) -> runFindS lsRow
+
+
+runFindS :: [[Text]] -> IO ()
+runFindS lsRow
+ = print $ H.findS $ map slurpRow lsRow
+
+
+slurpRow :: [Text] -> (H.Instance Text, Bool)
+slurpRow txs
+ = ( H.Instance $ tail txs
+   , case head txs of
+        "true"  -> True
+        "false" -> False
+        _       -> error "bad input data format")
+
